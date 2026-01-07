24f3c761810b8c56f03ffd8ba04a2fd61cb9d2b7
Rikas <rrikas341@gmail.com>
2026-01-07 20:08:07 +1100

    Create Input.s

---
+++ Input.s
@@ -0,0 +1,33 @@
+.data
+inputmessage: .asciiz "What's Your Name?"
+name: .space 10
+output1: .asciiz "Hello"
+output2: .asciiz "!Welcome to Assembly language Programme"
+
+.text
+_.globl main
+main: 
+la $a0,inputmessage
+li $v0,4
+syscall  
+	
+	la $a0,name
+	li $a1,10
+	li $v0,8
+	syscall  
+	
+	la $a0,output1
+	li $v0,4
+	syscall
+	
+	la $a0,name
+	li $v0,4
+	syscall  
+	
+	la $a0,output2
+	li $v0,4
+	syscall
+	
+    #Exit Program
+	li $v0, 10
+    syscall
